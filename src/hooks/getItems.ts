f448d528ca8d049d08aa99db363359797ae16952
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2024-02-06 22:39:46 +0100

    Clear code

--- src/hooks/getItems.ts
+++ src/hooks/getItems.ts
@@ -4,7 +4,6 @@ import { useQuery } from 'react-query';
 const getData = async () => {
   try {
     const response = await axios.get('http://localhost:1337/api/todos');
-    // console.log(response.data);
 
     return response.data.data;
   } catch (error) {
