c735521c82248dc42fe758f06c8b2686cae09171
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2024-02-06 19:19:13 +0100

    Fetching data from server & error handling(success)

--- src/hooks/getItems.ts
+++ src/hooks/getItems.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
+import { useQuery } from 'react-query';
 
 const getData = async () => {
   try {
     const response = await axios.get('http://localhost:1337/api/todos');
     // console.log(response.data);
 
-    return response.data;
+    return response.data.data;
   } catch (error) {
     throw new Error('Data not found');
   }
 };
+
+export const getItems = () => useQuery('todos', getData);
