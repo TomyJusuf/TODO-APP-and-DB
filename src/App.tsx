da7ed5bacc65a5832f3e13ea871d167455058325
TomasMatusek <Tomas.Matusek@edu.fh-kaernten.ac.at>
2024-02-06 19:14:21 +0100

    Use hooks getItems.ts & error - load handling. Send data as props importData to component BodyTodoTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import AddBarPanel from './components/AddBarPanel';
 import BodyTodoTable from './components/BodyTodoTable';
 import NavbarPanel from './components/NavbarPanel';
+import { getItems } from './hooks/getItems';
 import './index.css';
 
 function App() {
+  const { data, isLoading, isError } = getItems();
   return (
     <>
-      <NavbarPanel />
-      <AddBarPanel />
-      <BodyTodoTable />
+      {data && (
+        <>
+          <NavbarPanel />
+          <AddBarPanel />
+          <BodyTodoTable importData={data} />
+        </>
+      )}
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error 404 page not found</p>}
     </>
   );
 }
